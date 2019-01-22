0a6a7ef2703d5bcd5eb370c21f6de4d7c6270c5c
lujiaxing <lujiaxing@kaiyuan.net>
2019-01-22 15:09:12 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        <h2>
+                            hello world!
+                        </h2>
+                    </p>
+                    <p className="App-intro">
+                        你可以在 <code>src/App.js</code> 文件中修改。
+                    </p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
